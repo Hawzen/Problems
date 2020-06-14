39d9069b4e7f3ba8b2cfb4cedf78e480d0b11f5b
Hawzen <43524721+Hawzen@users.noreply.github.com>
2020-06-14 08:29:47 +0300

    Add up to 20 problems

--- Haskell 99 Problems/1-10.hs
+++ Haskell 99 Problems/1-10.hs
@@ -1,9 +1,11 @@
 -- Problems: https://wiki.haskell.org/99_questions/1_to_10
 
+
 -- Problem 1
 prob1 :: [a] -> a
 prob1 [] = error "Empty list"
 prob1 xs = last xs
+
 
 -- Problem 2
 prob2 :: [a] -> a
@@ -12,6 +14,7 @@ prob2 xs
     | length xs == 1 = head xs
     | otherwise = let x = init xs in last x
 
+
 -- Problem 3
 prob3 :: [a] -> Int -> a
 prob3 [] i = error "Empty list"
@@ -19,37 +22,26 @@ prob3 xs i = case len of True -> xs !! (i - 1)
                          False -> error "Index out of range"
     where len = length xs >= i
 
+
 -- Problem 5
 prob5 :: [a] -> [a]
 prob5 [] = []
 prob5 xs = [last xs] ++ prob5 (init xs)
 
+
 -- Problem 6
 prob6 :: (Eq a) => [a] -> Bool
 prob6 xs = xs == prob5 xs
 
-{- 
-Problem 7
-(**) Flatten a nested list structure.
 
-Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+-- Problem 7 Have to know date types
+-- prob17 :: [[]]
 
-Example:
 
-* (my-flatten '(a (b (c d) e)))
-(A B C D E)
-Example in Haskell:
+-- Problem 8
+prob8 :: (Eq a) => [a] -> [a]
+prob8 xs = foldr (\x acc -> if (head acc) == x then acc else x:acc) [last xs] (init xs)
 
-We have to define a new data type, because lists in Haskell are homogeneous.
-
- data NestedList a = Elem a | List [NestedList a]
-λ> flatten (Elem 5)
-[5]
-λ> flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
-[1,2,3,4,5]
-λ> flatten (List [])
-[]
--}
 
 -- Problem 9
 prob9 :: (Eq a) => [a] -> [[a]]
--- Haskell 99 Problems/11-20.hs
+++ Haskell 99 Problems/11-20.hs
@@ -1,3 +1,9 @@
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+-- Problems: https://wiki.haskell.org/99_questions/1_to_10
+
+
 -- Need to learn data types or something for 11 - 13
 
 -- -- Problem 11
@@ -9,18 +15,58 @@
 -- prob11 = map (\xs -> if p xs then (length xs, head xs) else head xs) $ prob9 
 --             where p = 1<  length
 
+
 -- Problem 14
 prob14 :: [a] -> [a]
 prob14 = foldl1 (++) . map (\x -> [x,x])
+
 
 -- Problem 15
 prob15 :: (Enum a) => Int -> [a] -> [a]
 prob15 i =  let replicate x = take i [x,x..] 
             in foldl (\acc x -> acc ++ replicate x) [] 
 
--- I am dizzy now, return later
--- -- Problem 16
--- prob16 :: Int -> [a] -> [[a]]
--- prob16 i =  let p acc = length acc < i
---             in foldl (\(hd:la) x -> if p hd then x:hd else x:head la) [[]]
--- -- prob16 i = takeWhile length 
+
+-- Problem 16
+prob16 :: Int -> [a] -> [a]
+prob16 m = 
+            let condition i = (i `mod` m) == 0
+                checkAndExecute = (\acc (i, x) -> if condition i then acc else x:acc)
+            in reverse . foldl checkAndExecute [] . zip [1,2..]
+
+
+-- Problem 17
+prob17 :: Int -> [a] -> ([a], [a])
+prob17 m = 
+        let sndAppend x acc = (fst acc, x:(snd acc))
+            fstAppend x acc= (x:(fst acc), snd acc)
+            checkAndAppend = (\acc (i, x) -> if m<i then sndAppend x acc else fstAppend x acc)
+            rev acc = (reverse (fst acc), reverse (snd acc))
+        in rev . foldl checkAndAppend ([], []) . zip [1,2..]
+
+        
+prob18 :: [a] -> Int -> Int -> [a]
+prob18 xs i j = 
+        let condition (_, m) = i <= m && m <= j
+        in map fst $ ( filter condition (zip xs [1..]))
+
+
+prob19 :: [a] -> Int -> [a]
+prob19 xs i =
+        let len = length xs
+            condition i n = mod (n+(-i)) len
+            keys = map (condition i)  [0..len-1]
+            dict = Map.fromList $ zip keys xs
+        in [dict Map.! key | key <- [0..len-1]]
+
+
+prob20 :: Int -> [a] -> (a, [a])
+prob20 m xs
+        | length xs <= m = error "Ya broke it! list too small"
+        | m <= 0 = error "Ya broke it! bad index"
+        | otherwise =
+            let list = (zip [1..] xs)
+                acc = (xs !! (m-1), [])
+                removeM = (\(i, x) acc -> if i==m-1 then acc else (fst acc, x:snd acc))
+            in (foldr removeM acc list)
+
